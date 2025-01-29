064db67113d3712218fc7d4f18f034cdaeda2b79
Morten Just <morten.just@gmail.com>
2025-01-29 15:48:54 +0100

    reproduce with nsview

--- Overlay Problem/ViewController.swift
+++ Overlay Problem/ViewController.swift
@@ -7,13 +7,34 @@
 
 import Cocoa
 import SwiftUI
+import SceneKit
 
 class ViewController: NSViewController {
+    @IBOutlet weak var sceneView: SCNView!
+    
+    @IBOutlet weak var customView: NSView!
+    
+    @objc func handleClick(sender: Any) {
+        print("Click!" + String(Date().timeIntervalSince1970))
+    }
+    
+    @objc func handlePan(sender: Any) {
+        print("Pan!" + String(Date().timeIntervalSince1970))
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Create an NSHostingView with the SwiftUI view
+        // add gesture recognizers to the sceneview (so we can create our own camera control)
+        let rec = NSClickGestureRecognizer(target: self, action: #selector(handleClick(sender: )) )
+        let panRec = NSPanGestureRecognizer(target: self, action: #selector(handlePan(sender:)))
+        
+        customView.addGestureRecognizer(rec)
+        customView.addGestureRecognizer(panRec)
+        
+        // set up the SwiftUI view as a subview of the view controller's view
+        // once this hostingview is added, the gesture recognizers no longer work
+        // it's as if the nshostingview/swiftui now swallows all events.
         let buttonView = ButtonView()
         let hostingView = NSHostingView(rootView: buttonView)
         
@@ -22,26 +43,50 @@ class ViewController: NSViewController {
         
         // Add it as a subview
         view.addSubview(hostingView)
-
+    }
+    
+    func createScene () -> SCNScene {
+        let box = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 1)
+        let node = SCNNode(geometry: box)
+        let scene = SCNScene()
+        scene.rootNode.addChildNode(node)
+        return scene
     }
 }
 
 
-// centered button in view
+
+
+
+// Centered button in transparent view.
 struct ButtonView: View {
     @State var count = 0
+    
     var body: some View {
-        VStack {
-            Text("Hello")
-            HStack {
-                Button("Swiftui Button" + String(count)) {
-                    count += 1
-                }
+        ZStack {
+            
+            // this has no effectÏ
+            // Color.clear.allowsHitTesting(false)
+            
+            Button("SwiftUI Button \(count)") {
+                count += 1
             }
-            Text("Hello")
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
         }
-        .background(Color.yellow)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
+
+
+
+// Using this instead of the NSHostingView does pass though all events
+class PassthroughHostingView<Content: View>: NSHostingView<Content> {
+//    override func hitTest(_ point: NSPoint) -> NSView? {
+//        // Return nil so events pass through this hosting view
+//        // Unless you want certain subareas to capture the event.
+//        return nil
+//    }
+}
