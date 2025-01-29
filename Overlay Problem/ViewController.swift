8a7ee3b1ae0938ecbbec83c91c5fc962837fec7c
Morten Just <morten.just@gmail.com>
2025-01-29 14:49:48 +0100

    Initial commit

--- Overlay Problem/ViewController.swift
+++ Overlay Problem/ViewController.swift
@@ -6,21 +6,42 @@
 //
 
 import Cocoa
+import SwiftUI
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create an NSHostingView with the SwiftUI view
+        let buttonView = ButtonView()
+        let hostingView = NSHostingView(rootView: buttonView)
+        
+        hostingView.frame = self.view.bounds
+        hostingView.autoresizingMask = [.height, .width]
+        
+        // Add it as a subview
+        view.addSubview(hostingView)
 
-        // Do any additional setup after loading the view.
     }
+}
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+
+// centered button in view
+struct ButtonView: View {
+    @State var count = 0
+    var body: some View {
+        VStack {
+            Text("Hello")
+            HStack {
+                Button("Swiftui Button" + String(count)) {
+                    count += 1
+                }
+            }
+            Text("Hello")
         }
+        .background(Color.yellow)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
-
-
 }
 
