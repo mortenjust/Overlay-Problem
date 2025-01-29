07575b311041b8e94aca0b86db03246c432118a2
Morten Just <morten.just@gmail.com>
2025-01-29 12:35:47 +0100

    Initial Commit

---
+++ Overlay Problem/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Overlay Problem
+//
+//  Created by Morten Petersen on 1/29/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Overlay ProblemTests/Overlay_ProblemTests.swift
@@ -0,0 +1,17 @@
+//
+//  Overlay_ProblemTests.swift
+//  Overlay ProblemTests
+//
+//  Created by Morten Petersen on 1/29/25.
+//
+
+import Testing
+@testable import Overlay_Problem
+
+struct Overlay_ProblemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
